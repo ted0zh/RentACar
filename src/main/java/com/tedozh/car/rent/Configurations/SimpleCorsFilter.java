5fa2392144c70483954016948cdbe968ffb0fd2d
ted0zh <tedi200220@gmail.com>
2023-12-16 14:56:28 +0200

    SimpleCorsFilter

---
+++ src/main/java/com/tedozh/car/rent/Configurations/SimpleCorsFilter.java
@@ -0,0 +1,27 @@
+package com.tedozh.car.rent.Configurations;
+import jakarta.servlet.*;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
